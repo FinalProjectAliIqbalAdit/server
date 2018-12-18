a51ffd8bb92b4a1507a94dee6d045146de135cea
iqbalmaulanaardi <iqbalmaulana.ardi@gmail.com>
2018-12-18 15:11:59 +0700

    testing not done

--- models/MeetingModel.js
+++ models/MeetingModel.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const MeetingSchema = new Schema({
+  place: {
+		type: String,
+		default: ''
+	},
 	host : {
 		type: Schema.Types.ObjectId,
 		ref: 'User'
